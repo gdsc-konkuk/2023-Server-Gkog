1f1e0bb03b2423214a96e8e039f7a793a3a76313
Brian <bwook9908@gmail.com>
2023-12-05 12:27:22 +0900

    feat: testcode

--- src/main/java/com/gkog/prototype/test/controller/TestController.java
+++ src/main/java/com/gkog/prototype/test/controller/TestController.java
@@ -1,6 +1,7 @@
 package com.gkog.prototype.test.controller;
 
 
+import org.springframework.boot.web.server.Cookie;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
