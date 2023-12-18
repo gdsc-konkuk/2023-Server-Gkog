90dac9dd22bd6eb47f6f28b1f5b7b2589c871adf
singjun <shinsj4653@gmail.com>
2023-12-19 00:00:33 +0900

    feat: Add Location Entity

---
+++ src/main/java/com/gkog/prototype/test/entity/Location.java
@@ -0,0 +1,24 @@
+package com.gkog.prototype.test.entity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import lombok.Getter;
+import org.springframework.data.geo.Point;
+
+@Entity
+@Getter
+public class Location {
+
+    @Id @GeneratedValue
+    @Column(name = "location_id")
+    private Long id;
+
+    @Column(nullable = false, columnDefinition = "TEXT")
+    private String roadAddress;
+
+    @Column(nullable = false, columnDefinition = "GEOMETRY")
+    private Point geography;
+
+}
