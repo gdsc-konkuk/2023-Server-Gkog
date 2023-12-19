514193820ee7a7770c0005ccfa8a78637dce50bf
ekgns33 <ekgns00421@gmail.com>
2023-12-20 03:23:06 +0900

    feat: create GCPConfig for GCS imageupload

---
+++ src/main/java/com/gkog/prototype/global/config/GCPConfig.java
@@ -0,0 +1,34 @@
+package com.gkog.prototype.global.config;
+
+import com.google.auth.oauth2.GoogleCredentials;
+
+import com.google.cloud.storage.Storage;
+import com.google.cloud.storage.StorageOptions;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.io.ClassPathResource;
+
+import java.io.IOException;
+
+@Configuration
+public class GCPConfig {
+
+    @Value("${spring.cloud.gcp.storage.project-id}")
+    String projectId;
+
+    @Value("${spring.cloud.gcp.storage.resourcePath}")
+    String resourcePath;
+
+    @Bean
+    public Storage storage() throws IOException {
+        ClassPathResource resource = new ClassPathResource(resourcePath);
+        GoogleCredentials credentials = GoogleCredentials.fromStream(resource.getInputStream());
+
+        return StorageOptions.newBuilder()
+                .setProjectId(projectId)
+                .setCredentials(credentials)
+                .build()
+                .getService();
+    }
+}
