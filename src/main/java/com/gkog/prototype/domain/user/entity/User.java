7cba4f37d45139cb5127861482876e1986f4a2db
ekgns33 <ekgns00421@gmail.com>
2023-12-19 14:54:12 +0900

    feat: create User Entity

---
+++ src/main/java/com/gkog/prototype/domain/user/entity/User.java
@@ -0,0 +1,43 @@
+package com.gkog.prototype.domain.user.entity;
+
+import com.gkog.prototype.domain.user.enums.ProviderType;
+import jakarta.persistence.*;
+import lombok.*;
+import org.hibernate.annotations.DynamicInsert;
+import org.hibernate.annotations.DynamicUpdate;
+
+@Entity
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+@DynamicInsert
+@Builder
+@Table(name = "users")
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "user_id", nullable = false)
+    private Long userId;
+
+    @Column(name = "email")
+    private String email;
+
+    @Column(name = "nickname", nullable = false)
+    private String nickName;
+
+    @Column(name = "img_url")
+//    @ColumnDefault("")
+    private String imgUrl;
+
+
+    @Column(name = "provider_type")
+    private String providerType;
+
+    @PrePersist
+    void prePersist() {
+        this.providerType = this.providerType == null ? ProviderType.APPLE.getName() : this.providerType;
+    }
+
+
+}
